20f1f1fa6546e0d5c23ffce43b01e3e446029926
Eric Yan <ericyan676@gmail.com>
2022-06-15 00:08:42 -0700

    Randonly generate 500,000 client entries

--- client_device_capacity_gen.py
+++ client_device_capacity_gen.py
@@ -1,27 +1,35 @@
 import itertools
 import json
 import pickle
+import random
 
 """
 Utility function to combine FedScale client computation data with Measurement Lab client bandwidth data
 """
 
-"""Data from FedScale repo"""
+# Data from FedScale repo
 fedscale_client = {}
 with open('data/client_device_capacity', 'rb') as fin:
     fedscale_client = pickle.load(fin)
+fedscale_client_len = len(fedscale_client)
 fedscale_client = dict(itertools.islice(fedscale_client.items(), 10000))
 fedscale_client = [x for _, x in fedscale_client.items()]
 fedscale_client = sorted(fedscale_client, key=lambda i: float(i['communication']))
 
-"""Data from https://www.measurementlab.net/tests/ndt/"""
+# Data from https://www.measurementlab.net/tests/ndt/
 ul_dl_mlab_ndt = []
 with open('data/ul_dl_capacity.json') as fin:
     ul_dl_mlab_ndt = json.load(fin)
 ul_dl_mlab_ndt = [{'dl_kbps': float(i['dl_mbps']) * 1000, 'ul_kbps': float(i['ul_mbps']) * 1000} for i in ul_dl_mlab_ndt]
 ul_dl_mlab_ndt = sorted(ul_dl_mlab_ndt, key=lambda i: i['ul_kbps'] + i['dl_kbps'])
 
-merged_capacity = {i: fs | ul_dl for i, (fs, ul_dl) in enumerate(zip(fedscale_client, ul_dl_mlab_ndt))}
+# Combine the two client capacity data
+merged_capacity = [fs | ul_dl for fs, ul_dl in zip(fedscale_client, ul_dl_mlab_ndt)]
+random.shuffle(merged_capacity)
+
+# FedScale's data is 500,000 entries, hence we are extending merged_capacity by 490,000 entries by randomly selecting from our existing 10,000
+merged_capacity.extend(random.choices(merged_capacity, k=fedscale_client_len - len(merged_capacity)))
+merged_capacity = {i: cl for i, cl in enumerate(merged_capacity)}
 
 floc = 'data/client_device_capacity_ul_dl'
 with open(floc, 'wb') as fout:
